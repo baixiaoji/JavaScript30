0354125e98dfde7525e20aa042241400d15c4296
暑假项目 <1041874421@qq.com>
2017-02-12 14:07:52 +0800

    backword & forward

--- 11 - Custom Video Player/scripts-baixiaoji.js
+++ 11 - Custom Video Player/scripts-baixiaoji.js
@@ -8,13 +8,27 @@ const ranges = player.querySelectorAll(".player__slider")
 const buttons = player.querySelectorAll("[data-skip]")
 
 /*事件函数 */
-function play(){
-    if(video.paused){
-        video.play()
-    }else{
-        video.pause()
-    }
+function togglePlay(){
+    const method = video.paused ? "play":"pause";
+    video[method]();
 }
+/*改变button */
+function updateButton(){
+    const icon = video.paused ? '►' : '❚ ❚';
+    // console.log(icon);
+    toggle.textContent = icon;
+}
+/* 快进 & 回退 */
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip)
+}
+
+
+
 /*绑定事件 */
-video.addEventListener("click",play)
-toggle.addEventListener("click",play)
+video.addEventListener("click",togglePlay);
+video.addEventListener("play",updateButton);
+video.addEventListener("pause",updateButton);
+
+toggle.addEventListener("click",togglePlay);
+buttons.forEach(button => button.addEventListener("click",skip))

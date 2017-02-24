418dede945629785ed77cc3981f264665954650c
暑假项目 <1041874421@qq.com>
2017-02-24 14:40:54 +0800

    add the funtion getVideo paintToVideo takePhoto

--- 19 - Webcam Fun/scripts-baixiaoji.js
+++ 19 - Webcam Fun/scripts-baixiaoji.js
@@ -7,18 +7,43 @@ const snap = document.querySelector('.snap');
 function getVideo(){
     // open the camera 
     navigator.mediaDevices.getUserMedia({video:true,audio:false})
+    // return the Primose
     .then((localMediaStream) =>{
        console.log(localMediaStream)
         video.src = window.URL.createObjectURL( localMediaStream );
         video.play()
-   
     })
+    //报错
     .catch(err =>{
         console.error(`OH NO!!!`,err)
     })
 }
-
+// paint to canvas
 function paintToCanvas(){
-    const width = 
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height =height;
+
+    return setInterval(()=>{
+        ctx.drawImage(video,0,0,width,height);
+        const pixwls = ctx.getImageData(0,0,width,height)
+    },16)
 }
+// screenshot
+function takePhoto(){
+    // shot sound
+    snap.currentTime = 0;
+    snap.play();
+    // take the data out of the canvas
+    const data = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download","handsome")
+    link.innerHTML = `<img src="${data}" alt="Handsome Man">`
+    strip.insertBefore(link,strip.firstChild)
+}
+
 getVideo();
+
+video.addEventListener("canplay",paintToCanvas)
